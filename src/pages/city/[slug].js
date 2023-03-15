4d242f190ee44db4ead5e42b5a635bf63b9c64ae
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-15 02:57:10 -0600

    Fix

--- src/pages/city/[slug].js
+++ src/pages/city/[slug].js
@@ -37,33 +37,33 @@ import { useRouter } from 'next/router'
 // Context
 import { favoritesContext } from '../../context/FavoritesProvider';
 
-// export async function getStaticProps({ params: { slug } }) {
-//     const response = await fetch(`https://wanderlust-api-production.up.railway.app/api/v1/cities/slug/${slug}`)
-//     const citySelected = await response.json()
+export async function getStaticProps({ params: { slug } }) {
+    const response = await fetch(`https://wanderlust-api-production.up.railway.app/api/v1/cities/slug/${slug}`)
+    const citySelected = await response.json()
 
-//     return {
-//         props: {
-//             citySelected: citySelected.data
-//         },
-//     };
-// }
+    return {
+        props: {
+            citySelected: citySelected.data
+        },
+    };
+}
 
-// export async function getStaticPaths() {
-//     const response = await fetch('https://wanderlust-api-production.up.railway.app/api/v1/sitemap/cities')
-//     const cities = await response.json()
-//     const paths = cities.map((city) => (
-//         {
-//             params: {
-//                 slug: city.slug,
-//             },
-//         }
-//     ))
+export async function getStaticPaths() {
+    const response = await fetch('https://wanderlust-api-production.up.railway.app/api/v1/sitemap/cities')
+    const cities = await response.json()
+    const paths = cities.map((city) => (
+        {
+            params: {
+                slug: city.slug,
+            },
+        }
+    ))
 
-//     return {
-//         paths,
-//         fallback: true,
-//     }
-// }
+    return {
+        paths,
+        fallback: true,
+    }
+}
 
 export default function CityPage({ citySelected }) {
     // Hooks
