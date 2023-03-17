3aa68fbeeb0fd8afd0236eae8ca8b93236bde0f9
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-16 23:43:44 -0600

    UI Fix

--- src/components/cityComponents/Map.js
+++ src/components/cityComponents/Map.js
@@ -116,7 +116,7 @@ export default function PlacesMap({ coordinates, userPlacesToTry, setUserPlacesT
                 />
             ))}
             {/* <!-- drawer component --> */}
-            <div style={{ zIndex: 1000 }} className={`absolute top-0 ${selectedMarker ? "right-0" : "-right-80"} w-80 h-full p-4 transition-all bg-white dark:bg-gray-800`} tabIndex="-1">
+            <div style={{ zIndex: 1000 }} className={`absolute top-0 ${selectedMarker ? "right-0" : "-right-80"} w-80 h-screen p-4 transition-all bg-white dark:bg-gray-800`} tabIndex="-1">
                 <h5 id="drawer-label" className="inline-flex items-center mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">Place</h5>
                 <button type="button" onClick={closePlaceOverlay} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                     <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
