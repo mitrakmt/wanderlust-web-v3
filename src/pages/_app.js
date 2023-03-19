acf8e0c77291afb281803b00dbe7cca7e2fc28d7
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-19 13:24:48 -0600

    Add default seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
+import {DefaultSeo} from 'next-seo';
 
 // Hooks
 import { AuthProvider } from "../hooks/useAuth";
@@ -41,13 +42,6 @@ const roboto = Roboto({
 export default function MyApp({ Component, pageProps }) {
     return (
         <div className={`overflow-scroll h-full max-h-screen scroll-smooth ${roboto.variable} font-sans`}>
-            <Head>
-                <meta charSet="UTF-8" />
-                <title>Plan Your Dream Trip | Wanderlust App - Your Ultimate Travel Companion</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <meta name="description" content="Wanderlust App is your ultimate travel companion, helping you plan and organize every aspect of your trip. From finding the best flights and accommodations to creating custom itineraries based on your interests, our app makes travel planning easy and stress-free. Browse our city guides and blog for travel inspiration, connect with fellow digital nomads, and let our AI Assistant optimize your travel plans. Download Wanderlust App and start planning your dream trip today." />
-                <meta name="google-site-verification" content="4sf8YAE60JQ_X8eClR3Y6uw8HHH5DW_yzKuMxARrLM8" />
-            </Head>
             <AuthProvider>
                 <ThemeProvider>
                     <ToastsProvider>
@@ -62,6 +56,21 @@ export default function MyApp({ Component, pageProps }) {
                                                         <CommunityProvider>
                                                             <Layout>
                                                                 <ErrorBoundary>
+                                                                    <DefaultSeo
+                                                                        title="Plan Your Dream Trip | Wanderlust App - Your Travel Companion"
+                                                                        description="Wanderlust App is your ultimate travel companion, helping you plan and organize every aspect of your trip. From finding the best flights and accommodations to creating custom itineraries based on your interests, our app makes travel planning easy and stress-free. Browse our city guides and blog for travel inspiration, connect with fellow digital nomads, and let our AI Assistant optimize your travel plans. Download Wanderlust App and start planning your dream trip today."
+                                                                        openGraph={{
+                                                                            type: 'website',
+                                                                            locale: 'en_IE',
+                                                                            url: 'https://www.wanderlustapp.io/',
+                                                                            siteName: 'Wanderlust App',
+                                                                        }}
+                                                                        twitter={{
+                                                                            handle: '@wanderlustext',
+                                                                            site: '@wanderlustext',
+                                                                            cardType: 'summary_large_image',
+                                                                        }}
+                                                                    />
                                                                     <Component {...pageProps} />
                                                                     <Analytics />
                                                                 </ErrorBoundary>
