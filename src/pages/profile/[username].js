efc79405ba49de58200ae410ddfa120487418bd3
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-24 22:33:29 -0600

    Add metadata for Profiles

--- src/pages/profile/[username].js
+++ src/pages/profile/[username].js
@@ -1,5 +1,4 @@
 import Profile from '../../shared_pages/profile';
-import { SocialProfileJsonLd } from 'next-seo';
 
 // Update for reviews 
 // Revalidate? or something
@@ -79,10 +78,13 @@ export default function PublicProfile({ recommendedLocations }) {
                     cardType: 'summary_large_image',
                 }}
             /> */}
-            {/* <SocialProfileJsonLd
-                type="Person"
-                name={}
-            /> */}
+            <CustomHead 
+                title="Your Personal Wanderlust App Profile | Edit Your Information"
+                description="Your Wanderlust App Profile is your personal space to save and access your travel information. Edit your profile to include your travel preferences and destinations, and view your saved trips and favorite places. Customize your experience and make the most of your travels with Wanderlust App Profile."
+                url="https://www.wanderlustapp.io/profile"
+                image="https://uploads-ssl.webflow.com/62893f6b6c73c80d757c8d0d/629378f07e3c95055ebae0ca_Screen%20Shot%202022-05-29%20at%204.38.07%20PM%20(1).jpg"
+                alt="Wanderlust App"
+            />
 
             <Profile publicUser={true} recommendedLocations={recommendedLocations} />
         </>
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,5 +1,6 @@
 import Profile from '../../shared_pages/profile';
 import Head from 'next/head';
+import CustomHead from '@/shared_components/CustomHead';
 
 export async function getStaticProps() {
     const response = await fetch(`https://wanderlust-api-production.up.railway.app/api/v1/locations/recommendations`)
@@ -16,29 +17,14 @@ export async function getStaticProps() {
 export default function ProfilePage({ recommendedLocations }) {
     return (
         <>
-            <Head>
-                <title>Your Personal Wanderlust App Profile | Edit Your Information</title>
+            <CustomHead
+                title="Your Personal Wanderlust App Profile | Edit Your Information"
+                description="Your Wanderlust App Profile is your personal space to save and access your travel information. Edit your profile to include your travel preferences and destinations, and view your saved trips and favorite places. Customize your experience and make the most of your travels with Wanderlust App Profile."
+                url="https://www.wanderlustapp.io/profile"
+                image="https://uploads-ssl.webflow.com/62893f6b6c73c80d757c8d0d/629378f07e3c95055ebae0ca_Screen%20Shot%202022-05-29%20at%204.38.07%20PM%20(1).jpg"
+                alt="Wanderlust App"
+            />
 
-            </Head>
-
-            <Head>
-                <title>Your Personal Wanderlust App Profile | Edit Your Information</title>
-                <meta name="description" content="Your Wanderlust App Profile is your personal space to save and access your travel information. Edit your profile to include your travel preferences and destinations, and view your saved trips and favorite places. Customize your experience and make the most of your travels with Wanderlust App Profile." />
-
-                {/* <!-- Open Graph / Facebook --> */}
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="Your Personal Wanderlust App Profile | Edit Your Information" />
-                <meta property="og:url" content={`https://www.wanderlustapp.io/profile`} />
-                <meta property="og:description" content="Your Wanderlust App Profile is your personal space to save and access your travel information. Edit your profile to include your travel preferences and destinations, and view your saved trips and favorite places. Customize your experience and make the most of your travels with Wanderlust App Profile." />
-                {/* <meta property="og:image" content={blog?.image_url} /> */}
-
-                {/* <!-- Twitter --> */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:title" content="Your Personal Wanderlust App Profile | Edit Your Information" />
-                <meta property="twitter:url" content={`https://www.wanderlustapp.io/profile`} />
-                <meta property="twitter:description" content="Your Wanderlust App Profile is your personal space to save and access your travel information. Edit your profile to include your travel preferences and destinations, and view your saved trips and favorite places. Customize your experience and make the most of your travels with Wanderlust App Profile." />
-                {/* <meta property="twitter:image" content={blog?.image_url} /> */}
-            </Head>
             <Profile publicUser={false} recommendedLocations={recommendedLocations} />
         </>
     )
