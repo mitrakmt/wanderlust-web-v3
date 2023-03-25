d80ef16e39e7c26a8c275e1d3a50abeba3375fd2
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-24 18:56:58 -0600

    remove app meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
-import { useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head';
 import Script from 'next/script'
 
 // Hooks
 import { AuthProvider } from "../hooks/useAuth";
-import { useRouter } from "next/router";
 
 // Context
 import FeaturedCitiesProvider from '../context/FeaturedCitiesProvider';
@@ -75,24 +72,6 @@ export default function MyApp({ Component, pageProps }) {
                                                         <CommunityProvider>
                                                             <Layout>
                                                                 <ErrorBoundary>
-                                                                    <Head>
-                                                                        <title>Plan Your Dream Trip | Wanderlust App - Your Travel Companion</title>
-                                                                        <meta key="viewport" name="viewport" content="initial-scale=1.0, width=device-width" />
-                                                                        <meta key="description" name="description" content="Wanderlust App is your ultimate travel companion, helping you plan and organize every aspect of your trip. From finding the best flights and accommodations to creating custom itineraries based on your interests, our app makes travel planning easy and stress-free. Browse our city guides and blog for travel inspiration, connect with fellow digital nomads, and let our AI Assistant optimize your travel plans. Download Wanderlust App and start planning your dream trip today." />
-                                                                        <meta key="image" property="og:image" content="https://uploads-ssl.webflow.com/62893f6b6c73c80d757c8d0d/629378f07e3c95055ebae0ca_Screen%20Shot%202022-05-29%20at%204.38.07%20PM%20(1).jpg" />
-                                                                        <meta key="width" property="og:image:width" content="800" />
-                                                                        <meta key="height" property="og:image:height" content="600" />
-                                                                        <meta key="alt" property="og:image:alt" content="Wanderlust App" />
-                                                                        <meta key="type" property="og:type" content="website" />
-                                                                        <meta key="locale" property="og:locale" content="en_IE" />
-                                                                        <meta key="url" property="og:url" content="https://www.wanderlustapp.io/" />
-                                                                        <meta key="site_name" property="og:site_name" content="Wanderlust App" />
-                                                                        <meta key="card" name="twitter:card" content="summary_large_image" />
-                                                                        <meta key="site" name="twitter:site" content="@wanderlustext" />
-                                                                        <meta key="creator" name="twitter:creator" content="@wanderlustext" />
-                                                                        <meta key="image" property="twitter:image" content="https://uploads-ssl.webflow.com/62893f6b6c73c80d757c8d0d/629378f07e3c95055ebae0ca_Screen%20Shot%202022-05-29%20at%204.38.07%20PM%20(1).jpg" />
-                                                                        <meta key="alt" property="twitter:image:alt" content="Wanderlust App" />
-                                                                    </Head>
                                                                     <Component {...pageProps} />
                                                                     <Analytics />
                                                                 </ErrorBoundary>
