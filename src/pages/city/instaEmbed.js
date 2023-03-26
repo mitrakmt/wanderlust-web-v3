845b4ede61f81fcfabffec454c38cd736200c4ab
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-03-26 10:00:14 -0600

    Bug fix

--- src/pages/city/instaEmbed.js
+++ src/pages/city/instaEmbed.js
@@ -5,7 +5,7 @@ const InstagramPost = ({ links }) => {
         <div className="w-full flex gap-x-4 justify-center flex-wrap overflow-hidden" style={{ height: '459px' }}>
             {
                 links && links?.map((link, index) => (
-                    <iframe key={`instagram-link-${link}-${index}`} className={`rounded-lg ${index === 0 ? "first:block hidden sm:block" : index === 1 ? "not:first-child hidden sm:block" : "not:first-child hidden md:block"}`} src={`https://www.instagram.com/p/${"CqO2I0OBfMK"}/embed`} width="325" height="100%" frameBorder="0" scrolling="no" allowFullScreen={true}></iframe>
+                    <iframe key={`instagram-link-${link}-${index}`} className={`rounded-lg ${index === 0 ? "first:block hidden sm:block" : index === 1 ? "not:first-child hidden sm:block" : "not:first-child hidden md:block"}`} src={`https://www.instagram.com/p/${link}/embed`} width="325" height="100%" frameBorder="0" scrolling="no" allowFullScreen={true}></iframe>
                 ))
             }
         </div>
