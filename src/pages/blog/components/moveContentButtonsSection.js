1d29df112e43ffaac7508eae03bb53e943b9d923
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-04-04 22:40:40 +0200

    Change styling of blog edit buttons

--- src/pages/blog/components/moveContentButtonsSection.js
+++ src/pages/blog/components/moveContentButtonsSection.js
@@ -2,12 +2,12 @@ export default function MoveContentButtonsSection({ index, content, handleMoveSe
     return (
         <div className="flex ml-auto">
             {
-                index !== content?.length - 1 && <button onClick={() => handleMoveSection(index, index + 1)} className="ml-2 px-2 py-1 text-sm font-medium text-white bg-primary-500 rounded-lg hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                index !== content?.length - 1 && <button onClick={() => handleMoveSection(index, index + 1)} className="flex items-center justify-center w-14 mr-1 px-2.5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                     <svg data-accordion-icon className="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
             }
             {
-                index !== 0 && <button onClick={() => handleMoveSection(index, index - 1)} className="ml-2 px-2 py-1 text-sm font-medium text-white bg-primary-500 rounded-lg hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+                index !== 0 && <button onClick={() => handleMoveSection(index, index - 1)} className="flex items-center justify-center w-14 mr-4 px-2.5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                     <svg data-accordion-icon className="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
             }
