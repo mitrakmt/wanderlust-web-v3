8cc9ef9f26ce9fc473ebb41171ef3fa56a78ff7e
Michael Mitrakos <mike.mitrakos@gmail.com>
2023-04-08 22:54:12 +0200

    Send to mobile feature

--- src/shared_components/CityDrawer.js
+++ src/shared_components/CityDrawer.js
@@ -8,11 +8,14 @@ export default function PlacesMap({ updateFavoritesList, currentFavorites = [], 
     const [showMobileTerms, setShowMobileTerms] = useState(false);
     const [acceptedMobileTerms, setAcceptedMobileTerms] = useState(false);
     const [isAlreadyFavorited, setIsAlreadyFavorited] = useState(false);
+    const [mobileMessageSentSuccess, setMobileMessageSentSuccess] = useState(false);
+    const [mobileSendInProgress, setMobileSendInProgress] = useState(false);
 
     // UseEffects
     useEffect(() => {
         // Search currentFavorites for selectedMarker.id
         if (selectedMarker) {
+            setMobileMessageSentSuccess(false);
             if (hasMarkerId(currentFavorites, selectedMarker.google_id)) {
                 setIsAlreadyFavorited(true)
             } else {
@@ -31,6 +34,15 @@ export default function PlacesMap({ updateFavoritesList, currentFavorites = [], 
     }
 
     const sendPlaceToMobile = () => {
+        if (mobileSendInProgress) {
+            return;
+        }
+        
+        setMobileSendInProgress(true);
+        if (!acceptedMobileTerms) {
+            return;
+        }
+
         request(`/sms/place`, {
             method: 'POST',
             body: JSON.stringify({
@@ -41,6 +53,12 @@ export default function PlacesMap({ updateFavoritesList, currentFavorites = [], 
                 note: selectedMarker.note,
                 google_place_id: selectedMarker.google_id
             })
+        }).then(status => {
+            if (status.data.success) {
+                // Show success message, disable button
+                setMobileMessageSentSuccess(true)
+            }
+            setMobileSendInProgress(false);
         })
     }
 
@@ -104,10 +122,13 @@ export default function PlacesMap({ updateFavoritesList, currentFavorites = [], 
                     }
                 </div>
 
-                {/* <div className="mt-4 mb-8">
+                <div className="mt-4 mb-8">
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sent to Mobile</label>
-                        <input type="confirm-password" onChange={(e) => setMobileNumber(e.target.value)} name="confirm-password" id="confirm-password" placeholder="Phone number" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-primary-500" />
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Send to Mobile</label>
+                        <input type="confirm-password" onChange={(e) => {
+                            setMobileNumber(e.target.value)
+                            setMobileMessageSentSuccess(false)
+                        }} name="confirm-password" id="confirm-password" placeholder="Phone number" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-primary-500" />
                     </div>
                     <div className="flex items-start my-2">
                         <div className="flex items-center h-5">
@@ -117,8 +138,19 @@ export default function PlacesMap({ updateFavoritesList, currentFavorites = [], 
                             <label onClick={() => setShowMobileTerms(true)} className="cursor-pointer font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500">Terms and Conditions</a></label>
                         </div>
                     </div>
-                    <button type="submit" onClick={sendPlaceToMobile} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Send</button>
-                </div> */}
+                    {
+                        mobileMessageSentSuccess ?
+                            <button className="cursor-auto w-full text-white bg-green-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sent</button>
+                            : <button type="submit" onClick={sendPlaceToMobile} className="w-full flex items-center justify-center text-white bg-primary-600 hover:bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">{mobileSendInProgress ? 
+                                <div role="status">
+                                    <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                    </svg>
+                                    <span className="sr-only">Loading...</span>
+                                </div> : "Send"}</button>
+                    }
+                </div>
 
                 <div className="flex justify-center w-full pb-4 space-x-4 md:px-4 mt-4">
                     <button type="button" onClick={closePlaceOverlay} className="inline-flex w-full justify-center text-gray-500 items-center bg-white hover:bg-gray-100 focus:outline-none rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
